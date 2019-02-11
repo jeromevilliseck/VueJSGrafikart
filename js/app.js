b2a84d74750eae52f400356f92a3479e0649a82a
Jerome Villiseck <jvk@abes.fr>
2019-02-11 14:28:58 +0100

    2/16 Decouverte 11:00

---
+++ js/app.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Salut les gens',
+        link: 'http://grafikart.fr',
+        success: true,
+        persons: ['je', 'mez', 'kel']
+    }
+});
